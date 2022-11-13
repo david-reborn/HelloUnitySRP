582594e68a0ddd39e8d674e5b38eebc32ea73879
david <3357346@qq.com>
2022-11-13 23:01:01 +0800

    Init Commit

---
+++ Assets/SRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,87 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+	ScriptableRenderContext context;
+
+	CullingResults cullingResults;
+
+	Camera camera;
+
+	const string bufferName = "Render Camera";
+
+	CommandBuffer buffer = new CommandBuffer
+	{
+		name = bufferName
+	};
+
+	static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+	public void Render(ScriptableRenderContext context, Camera camera)
+	{
+		this.context = context;
+		this.camera = camera;
+
+		if (!Cull())
+		{
+			return;
+		}
+		Setup();
+		DrawVisibleGeometry();
+		Submit();
+	}
+
+	void Setup()
+	{
+		buffer.BeginSample(bufferName);
+		buffer.ClearRenderTarget(true, true, Color.clear);
+		ExecuteBuffer();
+		context.SetupCameraProperties(camera);
+	}
+
+
+
+	void DrawVisibleGeometry()
+	{
+		var sortingSettings = new SortingSettings(camera)
+		{
+			criteria = SortingCriteria.CommonOpaque
+		};
+		var drawingSettings = new DrawingSettings(
+			unlitShaderTagId, sortingSettings
+		);
+
+		var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+		context.DrawRenderers(
+			cullingResults, ref drawingSettings, ref filteringSettings
+		);
+		context.DrawSkybox(camera);
+	}
+
+	void Submit()
+	{
+		buffer.EndSample(bufferName);
+		ExecuteBuffer();
+		context.Submit();
+	}
+
+	void ExecuteBuffer()
+	{
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+
+	bool Cull()
+    {
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+		{
+			cullingResults = context.Cull(ref p);
+			return true;
+		}
+		return false;
+	}
+}
